e3f79a08260b6fe3837bbbce1f1c36e15111ef70
wkgus0618 <wkgus0618@naver.com>
2023-08-25 16:59:29 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
+import java.util.Properties
+
 plugins {
-    java
-    `maven-publish`
+    id("java")
     id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+buildscript {
+    repositories { gradlePluginPortal() }
+    dependencies {
+        classpath("gradle.plugin.com.github.johnrengelman", "shadow", "7.1.2")
+    }
 }
 
 group = "com.iridium"
@@ -20,33 +28,34 @@ repositories {
 
 dependencies {
     // Dependencies that we want to shade in
-    implementation("org.jetbrains:annotations:24.0.1")
-    implementation("com.j256.ormlite:ormlite-core:6.1")
-    implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumTeams:2.1.4")
+    implementation("org.jetbrains", "annotations", "24.0.1")
+    implementation("com.j256.ormlite", "ormlite-core", "6.1")
+    implementation("com.j256.ormlite", "ormlite-jdbc", "6.1")
+    implementation("com.iridium", "IridiumTeams", "2.1.4")
 
     // Other dependencies that are not required or already available at runtime
-    compileOnly("org.projectlombok:lombok:1.18.26")
-    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    compileOnly("net.ess3:EssentialsXSpawn:2.16.1")
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.13-SNAPSHOT")
+    compileOnly("org.projectlombok", "lombok", "1.18.26")
+    compileOnly("org.spigotmc", "spigot-api", "1.19.4-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl", "VaultAPI", "1.7")
+    compileOnly("net.ess3", "EssentialsXSpawn", "2.16.1")
+    compileOnly("com.sk89q.worldedit", "worldedit-bukkit", "7.2.13-SNAPSHOT")
+}
 
-    // Enable lombok annotation processing
-    annotationProcessor("org.projectlombok:lombok:1.18.26")
+// Set the Java version and vendor
+val targetJavaVersion = 17
+java {
+    val javaVersion: JavaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
 }
 
 tasks {
     // "Replace" the build task with the shadowJar task (probably bad but who cares)
-    jar {
-        dependsOn("shadowJar")
-        enabled = false
-    }
+    build { dependsOn(shadowJar) }
 
-    shadowJar {
+    withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
         fun relocate(origin: String) =
                 relocate(origin, "com.iridium.iridiumskyblock.dependencies${origin.substring(origin.lastIndexOf('.'))}")
-
         // Remove the archive classifier suffix
         archiveClassifier.set("")
 
@@ -60,41 +69,37 @@ tasks {
     }
 
     // Set UTF-8 as the encoding
-    compileJava {
-        options.encoding = "UTF-8"
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(targetJavaVersion)
+    }
+
+    withType<Jar> {
+        val names = project.gradle.startParameter.taskNames
+        val name = if (names.size == 0) "build" else names[0]
+
+        val exc = "The build path doesn't exist. Build it on the default path."
+        val localProperties = project.rootProject.file("local.properties")
+        try {
+            val properties = Properties()
+            val stream = localProperties.inputStream()
+            properties.load(stream)
+
+            val buildDir = properties.getProperty("${name}Dir")
+            if (buildDir != null && buildDir.isNotBlank()) {
+                this.destinationDirectory.set(file(buildDir))
+            } else println(exc)
+            stream.close()
+        } catch (ignored: Exception) {
+            localProperties.writeText("buildDir=\ndeployDir=\n")
+            println(exc)
+        }
     }
 
     // Process Placeholders for the plugin.yml
     processResources {
-        filesMatching("**/plugin.yml") {
-            expand(rootProject.project.properties)
+        filesMatching("plugin.yml") {
+            expand("version" to project.version, "description" to project.description)
         }
-
-        // Always re-run this task
-        outputs.upToDateWhen { false }
-    }
-
-    compileJava {
-        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-        targetCompatibility = JavaVersion.VERSION_1_8.toString()
-    }
-
-    compileTestJava {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
-    }
-}
-
-// Set the Java version and vendor
-java {
-    toolchain {
-        vendor.set(JvmVendorSpec.ADOPTOPENJDK)
-    }
-}
-
-// Maven publishing
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
     }
 }
